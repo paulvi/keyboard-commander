ebb9674cbd36e506cbda5a4a7eb8f2c8eecb5a3b
Thomas J Bradley <theman@thomasjbradley.ca>
2012-04-10 14:45:33 -0400

    Added stubbed in game object.

---
+++ keyboard-commander.js
@@ -0,0 +1,36 @@
+var KeyboardCommander = (function () {
+  function start () {
+
+  }
+
+  function update () {
+
+  }
+
+  function success () {
+
+  }
+
+  function failure () {
+
+  }
+
+  function levelSuccess () {
+
+  }
+
+  function levelFailure () {
+
+  }
+
+  return {
+    start : start
+    , update : update
+    , success : success
+    , failure : failure
+    , levelSuccess : levelSuccess
+    , levelFailure : levelFailure
+  }
+}())
+
+Gamifer.registerGameObject(KeyboardCommander)
