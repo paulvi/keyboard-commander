2cdfd8549fe9b6f1f0744b1bef5a64c4636e587e
Thomas J Bradley <theman@thomasjbradley.ca>
2012-04-10 19:54:57 -0400

    Added level 4.

---
+++ levels/level-4.js
@@ -0,0 +1,55 @@
+var Level4 = (function () {
+  var levelId = 'level-4'
+    , levelForm = null
+    , formSubmitted = false
+
+    , winInput = null
+    , tabInput = null
+    , findInput = null
+
+  function update () {
+    var status = {status : 'continue'}
+
+    if (
+      formSubmitted
+      && winInput.value.toLowerCase() == 'w'
+      && tabInput.value.toLowerCase() == 't'
+      && findInput.value.toLowerCase() == 'f'
+    )
+      status.status = 'success'
+
+    return status
+  }
+
+  function startUp () {
+    levelForm = document.getElementById(levelId + '-form')
+    levelForm.addEventListener('submit', onFormSubmit, false)
+
+    winInput = document.getElementById('shortcut-win')
+    tabInput = document.getElementById('shortcut-tab')
+    findInput = document.getElementById('shortcut-find')
+  }
+
+  function cleanUp () {
+    levelForm.removeEventListener('submit', onFormSubmit)
+    levelForm = null
+
+    winInput = null
+    tabInput = null
+    findInput = null
+  }
+
+  function onFormSubmit (ev) {
+    ev.preventDefault()
+
+    formSubmitted = true
+  }
+
+  return {
+    levelId : levelId
+    , update : update
+    , startUp : startUp
+  }
+}())
+
+Gamifier.registerLevelObject(Level4)
