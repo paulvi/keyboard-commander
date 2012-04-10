07ee84a8a9204af0215bd2ee0ff74702cd7f886b
Thomas J Bradley <theman@thomasjbradley.ca>
2012-04-10 19:27:59 -0400

    Added success screen.

--- keyboard-commander.js
+++ keyboard-commander.js
@@ -1,4 +1,9 @@
 var KeyboardCommander = (function () {
+  var activeScreen = document.getElementById('active-screen')
+    , inactiveScreens = document.getElementById('inactive-screens')
+    , levelSuccessScreen = document.getElementById('level-success')
+    , successTimeout = null
+
   function startScreen () {
     document.getElementsByClassName('game-start')[0].addEventListener('click', function gameStartClick (ev) {
       this.removeEventListener('click', gameStartClick)
@@ -26,7 +31,13 @@ var KeyboardCommander = (function () {
   }
 
   function levelSuccess () {
-    console.log('success')
+    activeScreen.appendChild(levelSuccessScreen)
+
+    successTimeout = setTimeout(function () {
+      clearTimeout(successTimeout)
+      inactiveScreens.appendChild(levelSuccessScreen)
+      Gamifier.nextLevel()
+    }, 1500)
   }
 
   function levelFailure () {
