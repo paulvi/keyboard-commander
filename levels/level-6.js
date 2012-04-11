d91d614d8c7c77526ee4216a9de22a49481990ed
Thomas J Bradley <theman@thomasjbradley.ca>
2012-04-10 20:25:52 -0400

    Added level 6.

---
+++ levels/level-6.js
@@ -0,0 +1,53 @@
+var Level6 = (function () {
+  var levelId = 'level-6'
+    , levelForm = null
+    , formSubmitted = false
+
+    , highlightInput = null
+
+  function update () {
+    var status = {status : 'continue'}
+
+    if (
+      formSubmitted
+      && highlightInput.value.toLowerCase() == 'shift'
+    )
+      status.status = 'success'
+
+    if (
+      formSubmitted
+      && highlightInput.value.toLowerCase() != 'shift'
+    )
+      status.status = 'failure'
+
+    return status
+  }
+
+  function startUp () {
+    levelForm = document.getElementById(levelId + '-form')
+    levelForm.addEventListener('submit', onFormSubmit, false)
+
+    highlightInput = document.getElementById('shortcut-highlight')
+  }
+
+  function cleanUp () {
+    levelForm.removeEventListener('submit', onFormSubmit)
+    levelForm = null
+
+    highlightInput = null
+  }
+
+  function onFormSubmit (ev) {
+    ev.preventDefault()
+
+    formSubmitted = true
+  }
+
+  return {
+    levelId : levelId
+    , update : update
+    , startUp : startUp
+  }
+}())
+
+Gamifier.registerLevelObject(Level6)
