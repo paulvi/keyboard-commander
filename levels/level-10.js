738cda628e49b618a79af78e5891a5b756c028f4
Thomas J Bradley <theman@thomasjbradley.ca>
2012-04-10 21:13:24 -0400

    Added level 10.

---
+++ levels/level-10.js
@@ -0,0 +1,44 @@
+var Level10 = (function () {
+  var levelId = 'level-10'
+    , linkFinish = null
+    , linkClicked = false
+
+    , ruleInput = null
+
+  function update () {
+    var status = {status : 'continue'}
+
+    if (
+      linkClicked
+      && ruleInput.checked
+    )
+      status.status = 'success'
+
+    return status
+  }
+
+  function startUp () {
+    linkFinish = document.getElementById('finish')
+    linkFinish.addEventListener('click', onLinkClick, false)
+    ruleInput = document.getElementById('rule')
+  }
+
+  function cleanUp () {
+    linkFinish = null
+    ruleInput = null
+  }
+
+  function onLinkClick (ev) {
+    ev.preventDefault()
+
+    linkClicked = true
+  }
+
+  return {
+    levelId : levelId
+    , update : update
+    , startUp : startUp
+  }
+}())
+
+Gamifier.registerLevelObject(Level10)
